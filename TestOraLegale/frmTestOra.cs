eeebb20d8dd0d4a3b419ee65f9e7441f050110fc
Fabio Rossetti <fabio.rossetti@gmail.com>
2018-12-26 17:01:08 +0100

    versione iniziale

---
+++ TestOraLegale/frmTestOra.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestOraLegale
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (TimeZoneInfo.Local.IsDaylightSavingTime(DateTime.Now))
+            {
+                label1.Text = "Ora legale";
+            }
+            else
+            {
+                label1.Text = "Ora solare";
+            }
+        }
+    }
+}
